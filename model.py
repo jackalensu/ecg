970450c7138b1bfcb52a26053a2178bdecd9316c
scsu <jackalensu@gmail.com>
2019-08-04 22:30:05 +0800

    add lstm layer and 24hr mode

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras.activations import softmax
 from keras import backend as keras
 
 
@@ -104,9 +105,79 @@ def unet_1d(pretrained_weights = None,input_size = (10000,8)):
     conv9 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv1D(6, 1, activation = 'softmax')(conv9)
+    
+    conv10 = LSTM(units = 8, return_sequences = True)(conv9)
+    conv11 = LSTM(units = 8, return_sequences = True)(conv10)
+    conv12 = LSTM(units = 6, return_sequences = True)(conv11)
+    conv13 = Softmax()(conv12)
+    
+    #conv10 = Conv1D(6, 1, activation = 'softmax')(conv10)
+    
 
-    model = Model(inputs = inputs, outputs = conv10)
+
+    model = Model(inputs = inputs, outputs = conv13)
+
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'categorical_crossentropy', metrics = ['accuracy'], sample_weight_mode="temporal")
+    
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
+
+def unet_1d_24hr(pretrained_weights = None,input_size = (5000,1)):
+    inputs = Input(input_size)
+    conv1 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling1D(pool_size=2)(conv1)
+    conv2 = Conv1D(16, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv1D(16, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling1D(pool_size=2)(conv2)
+    conv3 = Conv1D(32, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv1D(32, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling1D(pool_size=2)(conv3)
+    conv4 = Conv1D(64, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv1D(64, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    #drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling1D(pool_size=2)(conv4)
+
+    conv5 = Conv1D(128, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv1D(128, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    #drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv1D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = 2)(conv5))
+    up6 = ZeroPadding1D(padding=(1,0))(up6)
+    merge6 = concatenate([conv4,up6], axis = 2)
+    conv6 = Conv1D(64, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv1D(64, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv1D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = 2)(conv6))
+    merge7 = concatenate([conv3,up7], axis = 2)
+    conv7 = Conv1D(32, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv1D(32, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv1D(16, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = 2)(conv7))
+    merge8 = concatenate([conv2,up8], axis = 2)
+    conv8 = Conv1D(16, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv1D(16, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv1D(8, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling1D(size = 2)(conv8))
+    merge9 = concatenate([conv1,up9], axis = 2)
+    conv9 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv1D(8, 7, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    
+    #conv10 = Conv1D(2, 1, activation = 'softmax')(conv9)
+
+    conv10 = LSTM(units = 8, return_sequences = True)(conv9)
+    conv11 = LSTM(units = 8, return_sequences = True)(conv10)
+    conv12 = LSTM(units = 2, return_sequences = True)(conv11)
+    conv13 = Softmax()(conv12)
+    
+
+    model = Model(inputs = inputs, outputs = conv13)
 
     model.compile(optimizer = Adam(lr = 1e-4), loss = 'categorical_crossentropy', metrics = ['accuracy'], sample_weight_mode="temporal")
     
